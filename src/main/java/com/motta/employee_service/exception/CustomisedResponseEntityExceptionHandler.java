d11ccca36137ed83e527c11e244e1b97bdf7dc47
arunpt7777@gmail.com <arunpt7777@gmail.com>
2024-09-14 10:52:27 +0530

    US38 - Print logs of Response and Request using AOP

--- src/main/java/com/motta/employee_service/exception/CustomisedResponseEntityExceptionHandler.java
+++ src/main/java/com/motta/employee_service/exception/CustomisedResponseEntityExceptionHandler.java
@@ -57,6 +57,14 @@ public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 
 		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
-
 	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleResourceNotFoundException(Exception ex, WebRequest request)
+			throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+	}
+
 }
